23ccaeb3dbf53eb09ab369a2edff29ba5f3b15b1
Artem Vysotsky <a.vysotsky@gmail.com>
2023-06-19 19:11:39 +0300

    fix adapt images

--- public/index.js
+++ public/index.js
@@ -1,81 +1,5 @@
-// images config
-const images = {
-    host: document.querySelector('meta[name="images.host"]').content,
-    widths: document.querySelector('meta[name="images.widths"]').content.split(' ')
-}
 
-// adapt image
-function adaptImage(uri, width, height = null) {
-    const container = { width, height }
-    const size = uri.match(/\/(\d{3,4})x(\d{3,4})\.jpg$/)
-    const maximum = { width: parseInt(size[1]), height: parseInt(size[2]) }
-    const ratio = maximum.width / maximum.height
-    const adaptive = { width: parseInt(images.widths) }
-    for(const width of images.widths) {
-        const current = { width: parseInt(width) }
-        current.height = Math.floor(current.width / ratio)
-        if (current.width >= maximum.width) break
-        if (container.height) {
-            if ((current.width >= container.width)
-                    && (current.height > container.height)) {
-                break
-            }
-        } else {
-            if (current.width > container.width) break
-        }
-        adaptive.width = current.width
-    }
-    return images.host + uri
-        .replace(/\/\d{3,4}x\d{3,4}\.jpg$/, '/' + adaptive.width + '.jpg')
-}
-
-// adaptive lazyload slideshow
-const slideshow = document.querySelector('#home #slideshow')
-slideshow && slideshow.querySelectorAll('img').forEach(image => {
-    image.src = adaptImage(
-        image.dataset.url, slideshow.offsetWidth, slideshow.offsetHeight
-    )
-    return image
-})
-
-// lazyload article main background image adaptive
-const main = document.querySelector('article div.main[data-image]')
-if (main) {
-    main.style.backgroundImage = `url('${
-        adaptImage(
-            main.dataset.image, main.offsetWidth, main.offsetHeight
-        )
-    }')`
-}
-
-// lazyload article images adaptive
-const imagesObserver = new IntersectionObserver((entries, self) => {
-    for (const entrie of entries) {
-        if (!entrie.isIntersecting) continue
-        entrie.target.src = adaptImage(
-            entrie.target.dataset.url, entrie.target.offsetWidth
-        )
-        self.unobserve(entrie.target)
-    }
-}, {
-    root: null, rootMargin: '50px', threshold: 0
-})
-document.querySelectorAll('article img')
-.forEach(image => imagesObserver.observe(image))
-
-// lazyload more posts
-new IntersectionObserver((entries, self) => {
-    if (!entries[0].isIntersecting) return
-    const posts = entries[0].target.querySelectorAll('div.post')
-    posts.forEach(post => {  
-        const url = post.dataset.image.replace(/\/\d+x\d+\.jpg$/,'/320.jpg')
-        post.style.backgroundImage = `url(${images.host}/${url})`
-    })
-    self.disconnect();
-}, { root: null, rootMargin: '50px', threshold: 0 }).observe(
-    document.querySelector('#more')
-)
-
+/*
 // ajax api
 const api = {
     fetch: async (uri, params = {}) => {
@@ -121,7 +45,8 @@ api.post = async (uri, params = {}) => {
     }
     return api.fetch(uri, params)
 }
-
+*/
+/*
 // google map
 function initMap() {
     const mapNode = document.getElementById('map')
@@ -144,7 +69,7 @@ function initMap() {
         })
     })
 }
-
+*/
 window.onerror = function(errorMsg, url, lineNumber) {
     console.debug('window.onError: ', errorMsg, url, lineNumber)
     alert(errorMsg)
