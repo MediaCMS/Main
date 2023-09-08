4fb88a09cff801590cb75985319d63e7e61dbe3b
Artem Vysotsky <a.vysotsky@gmail.com>
2023-09-08 20:11:01 +0300

    replace sitemap

--- controllers/sitemap.js
+++ controllers/sitemap.js
@@ -1,88 +1,89 @@
-import fs from 'fs'
+import fs from 'fs';
 import db from '../db.js';
-import { SitemapStream } from 'sitemap';
 import config from '../config.js';
 
 export default async function (request, response) {
 
-    const sitemap = new SitemapStream({ hostname: config.host });
-    const stream = fs.createWriteStream(config.root + '/public/sitemap.xml');
+    const sitemap = new Sitemap();
 
-    sitemap.pipe(stream);
-    sitemap.write({ url: '/', changefreq: 'hourly', priority: 1.0 });
-
-    sitemap.write({ url: '/publikatsiyi', changefreq: 'monthly', priority: 0.8 });
+    sitemap.add('/', 'hourly', 1);
+    
+    sitemap.add('/publikatsiyi', 'hourly', 0.8);
     const posts = db.collection('posts')
         .find({ status: true })
         .sort({ time: 1 })
         .project({ _id: 0, slug: 1 });
     while (await posts.hasNext()) {
         const post = await posts.next();
-        sitemap.write({
-            url: `/publikatsiyi/${post.slug}`,
-            changefreq: 'never',
-            priority: 0.3
-        })
+        sitemap.add('/publikatsiyi/' + post.slug, 'never', 0.9);
     }
 
-    sitemap.write({ url: '/katehoriyi', changefreq: 'monthly', priority: 0.5 });
+    sitemap.add('/katehoriyi', 'yearly', 0.3);
     const categories = db.collection('categories')
         .find({ status: true })
         .sort({ order: 1 })
         .project({ _id: 0, slug: 1 });
     while (await categories.hasNext()) {
         const category = await categories.next();
-        sitemap.write({
-            url: `/katehoriyi/${category.slug}`,
-            changefreq: 'hourly',
-            priority: 0.3
-        })
+        sitemap.add('/katehoriyi/' + category.slug, 'hourly', 0.8);
     }
 
-    sitemap.write({ url: '/mitky', changefreq: 'monthly', priority: 0.5 });
+    sitemap.add('/mitky', 'monthly', 0.3);
     const tags = db.collection('tags')
         .find({ status: true })
         .sort({ title: 1 })
         .project({ _id: 0, slug: 1 });
     while (await tags.hasNext()) {
         const tag = await tags.next();
-        sitemap.write({
-            url: `/mitky/${tag.slug}`,
-            changefreq: 'hourly',
-            priority: 0.3
-        })
+        sitemap.add('/mitky/' + tag.slug, 'hourly', 0.8);
     }
 
-    sitemap.write({ url: '/korystuvachi', changefreq: 'monthly', priority: 0.1 });
+    sitemap.add('/korystuvachi', 'monthly', 0.3);
     const users = db.collection('users')
         .find({ status: true })
         .sort({ title: 1 })
         .project({ _id: 0, slug: 1 });
     while (await users.hasNext()) {
         const user = await users.next();
-        sitemap.write({ 
-            url: `/korystuvachi/${user.slug}`,
-            changefreq: 'never',
-            priority: 0.3
-        })
+        sitemap.add('/korystuvachi/' + user.slug, 'hourly', 0.8);
     }
 
-    sitemap.write({ url: '/storinky', changefreq: 'never', priority: 0.1 });
+    sitemap.add('/storinky', 'never', 0.1);
     const pages = db.collection('pages')
         .find({ status: true })
         .sort({ title: 1 })
         .project({ _id: 0, slug: 1 });
+
     while (await pages.hasNext()) {
         const page = await pages.next();
-        sitemap.write({
-            url: `/storinky/${page.slug}`,
-            changefreq: 'never',
-            priority: 0.3
-        })
+        sitemap.add('/never/' + page.slug, 'never', 0.1);
     }
 
-    sitemap.write({ url: '/poshuk', changefreq: 'never', priority: 0.1 });
-    
-    sitemap.end();
+    sitemap.add('/poshuk', 'never', 0.1);
+
+    fs.writeFileSync(config.root + '/public/sitemap.xml', sitemap.get());
+
     response.end();
+}
+
+class Sitemap {
+
+    constructor () {
+        this.xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        this.xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    }
+
+    get () {
+        return this.xml + '</urlset>';
+    }
+
+    add (url, changefreq = 'never', priority = 0) {
+        let item = '';
+        item += '<url>';
+        item += `<loc>${config.protocol}://${config.host}${url}</loc>`;
+        item += `<changefreq>${changefreq}</changefreq>`;
+        item += `<priority>${priority}</priority>`;
+        item += '</url>';
+        this.xml += item;
+    }
 }
