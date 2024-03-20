3ad7451a60d32eaa83a590c12d4391d642ae84b3
Artem Vysotsky <a.vysotsky@gmail.com>
2024-03-20 16:35:20 +0200

    add swipe test

--- public/index.js
+++ public/index.js
@@ -40,8 +40,58 @@ const imagesObserver = new IntersectionObserver((entries, self) => {
     root: null, rootMargin: '50px', threshold: 0
 })
 document.querySelectorAll('article img')
-.forEach(image => imagesObserver.observe(image))
+.forEach(image => imagesObserver.observe(image));
 
+
+// Swipe
+// https://www.kirupa.com/html5/detecting_touch_swipe_gestures.htm
+(() => {
+    const container = document.querySelector('body')
+    container.addEventListener('touchstart', startTouch, false)
+    container.addEventListener('touchmove', moveTouch, false)
+    let initialX = null;
+    let initialY = null;
+    function startTouch(e) {
+        initialX = e.touches[0].clientX;
+        initialY = e.touches[0].clientY;
+    }
+
+    function moveTouch(e) {
+        if (initialX === null) return;
+        if (initialY === null) return;
+    
+        var currentX = e.touches[0].clientX;
+        var currentY = e.touches[0].clientY;
+    
+        var diffX = initialX - currentX;
+        var diffY = initialY - currentY;
+    
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+          // sliding horizontally
+          if (diffX > 0) {
+            // swiped left
+            console.log("swiped left");
+          } else {
+            // swiped right
+            console.log("swiped right");
+          }  
+        } else {
+          // sliding vertically
+          if (diffY > 0) {
+            // swiped up
+            console.log("swiped up");
+          } else {
+            // swiped down
+            console.log("swiped down");
+          }  
+        }
+    
+        initialX = null;
+        initialY = null;
+    
+        e.preventDefault();
+    };
+})()
 
 window.onerror = function(errorMsg, url, lineNumber) {
     console.debug('window.onError: ', errorMsg, url, lineNumber)
